2c7ec70a07e951cd5ae7669173785f54df07068c
jack <jack@Fedora.localhost>
2013-04-03 18:24:07 +0800

    sql

---
+++ db.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.6.10, for Linux (i686)
+--
+-- Host: localhost    Database: db_chat
+-- ------------------------------------------------------
+-- Server version	5.6.10
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `info_user`
+--
+
+DROP TABLE IF EXISTS `info_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `info_user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `userPassword` varchar(50) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `info_user`
+--
+
+LOCK TABLES `info_user` WRITE;
+/*!40000 ALTER TABLE `info_user` DISABLE KEYS */;
+INSERT INTO `info_user` VALUES (1,'j','c08ac56ae1145566f2ce54cbbea35fa3'),(2,'jkk','33f27d1e53f646749e936448858920c1'),(3,'jk','74b9495fbf5ce041652eddb4254b9511'),(4,'jk','74b9495fbf5ce041652eddb4254b9511'),(5,'jk','74b9495fbf5ce041652eddb4254b9511'),(6,'jk','74b9495fbf5ce041652eddb4254b9511'),(7,'jk','74b9495fbf5ce041652eddb4254b9511'),(8,'k','c08ac56ae1145566f2ce54cbbea35fa3'),(9,'kl','c08ac56ae1145566f2ce54cbbea35fa3'),(10,'jkjj','74b9495fbf5ce041652eddb4254b9511'),(11,'jkjkjk','74b9495fbf5ce041652eddb4254b9511'),(12,'jkjk','74b9495fbf5ce041652eddb4254b9511'),(13,'jkjkjkjk','74b9495fbf5ce041652eddb4254b9511'),(14,'jkjkjkjkjk','74b9495fbf5ce041652eddb4254b9511'),(15,'jkjkjkjkjkjk','74b9495fbf5ce041652eddb4254b9511'),(16,'ji','3abf00fa61bfae2fff9133375e142416'),(17,'hj','ba984cb106d5bcead9bfe67344b89a2a'),(18,'hjhj','c1d2228aad6099939626ffbb05f034f8'),(19,'hjhjhj','c1d2228aad6099939626ffbb05f034f8'),(20,'kkkk','78d1fe0f0064cf6654940c87d99d4456'),(21,'0000','670b14728ad9902aecba32e22fa4f6bd'),(22,'hi','c4bb408471eb7727e59e11385b0a8c19'),(23,'00','670b14728ad9902aecba32e22fa4f6bd'),(24,'9','52c69e3a57331081823331c4e69d3f2e'),(25,'8','21218cca77804d2ba1922c33e0151105'),(26,'5','5b1b68a9abf4d2cd155c81a9225fd158'),(27,'6','f379eaf3c831b04de153469d1bec345e'),(28,'56','670b14728ad9902aecba32e22fa4f6bd'),(29,'90','df780a97b7d6a8f779f14728bccd3c4c'),(30,'9090','df780a97b7d6a8f779f14728bccd3c4c'),(31,'7','f63f4fbc9f8c85d409f2f59f2b9e12d5'),(32,'l','a73f86ae408af70b67141843e7130723'),(33,'78','5379884c5ec4e06879f7400fd40be0d9');
+/*!40000 ALTER TABLE `info_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-04-03 18:15:52
